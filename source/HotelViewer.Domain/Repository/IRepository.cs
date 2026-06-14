8d53c99752ab1b258a7578ccd3f488b24775b577
Urtyom-Alyanov <urtyomalyanov@gmail.com>
2026-06-15 01:27:01 +0300

    domain(repo, chore): fix naming convention

--- source/HotelViewer.Domain/Repository/IRepository.cs
+++ source/HotelViewer.Domain/Repository/IRepository.cs
@@ -1,15 +1,13 @@
-﻿using HotelViewer.Domain.Entity;
-using HotelViewer.Domain.Value;
-using LanguageExt;
+﻿using LanguageExt;
 
 namespace HotelViewer.Domain.Repository;
 
 public abstract record RepositoryError(string Message);
-public record EntityNotFound(object Id) : RepositoryError($"Сущность с ID {Id} не найдена в системе.");
+public record EntityNotFound<TEntityId>(TEntityId Id) : RepositoryError($"Сущность с ID {Id} не найдена в системе.");
 public record InfrastructureFault(string Detail) : RepositoryError($"Сбой инфраструктуры данных: {Detail}");
 
-public interface IRepository<Entity, EntityId>
+public interface IRepository<TEntity, in TEntityId>
 {
-    public Either<RepositoryError, Entity> GetById(EntityId id);
-    public Either<RepositoryError, Entity> Save(Entity entity);
+    public Either<RepositoryError, TEntity> FindById(TEntityId id);
+    public Either<RepositoryError, TEntity> Save(TEntity entity);
 }
